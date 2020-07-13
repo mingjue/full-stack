8f263cd3f37124b481b431efccf3773781771e91
Mingjue <mingjue@vt.edu>
2020-07-12 17:05:28 -0700

    Q1 done

--- hw2/01-fizzbuzz.js
+++ hw2/01-fizzbuzz.js
@@ -1,1 +1,23 @@
 // Enter your code here
+function fizzbuzz(){
+    for(var i = 1; i<=100; i++ ){
+        if(i % 3==0){
+            document.write(" fizz");
+            document.write("<br>");
+        }
+     else if( i % 5 == 0){
+        document.write(" buzz");
+        document.write("<br>");
+        }
+     else if( i % 15 == 0){
+        document.write(" fizzbuzz");
+        document.write("<br>");
+        }
+        else{
+            document.write(i);
+            document.write("<br>");
+        }
+    }
+}
+
+fizzbuzz()
