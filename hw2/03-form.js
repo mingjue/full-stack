22cee3c1389861ddacbd438c8d1eb2375ab09ce0
Mingjue <mingjue@vt.edu>
2020-07-12 17:39:26 -0700

    q3

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,1 +1,9 @@
 // Enter your code here
+function getinfo(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("else").value;
+    var checkbox = document.getElementById("checkbox").value;
+    
+    console.log("name: "+name+"\nemail: "+email+"\ncomments: "+message+"\nnewsletter: "+ checkbox);
+}
