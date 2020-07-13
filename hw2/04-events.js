4c201750fcaf0ca6a4f3ef6941830affa4e84c8e
Mingjue <mingjue@vt.edu>
2020-07-12 17:43:38 -0700

    q4

--- hw2/04-events.js
+++ hw2/04-events.js
@@ -1,1 +1,8 @@
 // Enter your code here
+function resize(){
+    w=window.innerWidth;
+    h=window.innerHeight;
+    
+    document.getElementById('width').innerHTML=w;
+    document.getElementById('height').innerHTML=h;
+}
