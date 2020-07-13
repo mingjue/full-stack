b276304f5301ab83345b8e15fcd0f58e84704f9e
Mingjue <mingjue@vt.edu>
2020-07-12 17:59:47 -0700

    q5

--- hw2/05-api.js
+++ hw2/05-api.js
@@ -1,3 +1,25 @@
 const url = 'https://restcountries.eu/rest/v2/all';
 
 // Enter your code here
+    
+fetch(url)
+   .then(response => { 
+        console.log('Request suceessful', response);
+
+        return response.json();
+    })
+    .then(data => {
+        document.write("<h2>REST Countries</h2>");
+        var i =0;
+        while(i<250){
+        num =  data[i].population.toString().split( /(?=(?:\d{3})+(?:\.|$))/g ).join( "," );
+        document.write(i+"." +" "+ data[i].name +' - '+ num );
+        document.write("<br>")
+        i++;
+        }
+    })
+    
+    .catch(error => { console.log('Request failed' , error )
+    });
+    
+    
