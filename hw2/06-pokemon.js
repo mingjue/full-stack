4e20b5eb3888941ca010037cba3235959d2b4abf
Mingjue <mingjue@vt.edu>
2020-07-12 18:52:12 -0700

    q6

--- hw2/06-pokemon.js
+++ hw2/06-pokemon.js
@@ -9,19 +9,14 @@ let getDataAsync = async url =>{
         let data = await response.json();
         var i=0;
         while(i<20){
-        /*
-        document.write(data.results[i].name);
-        document.write('\n');
-        */
-        console.log(data.results[i].name);
-        var upper = data.results[i].name.charAt(0).toUpperCase()+ data.results[i].name.slice(1);
-        var span = document.createElement('span');
-        var text = document.createTextNode(upper+ '-\n');
+            var upper = data.results[i].name.charAt(0).toUpperCase()+ data.results[i].name.slice(1);
+            var span = document.createElement('span');
+            var text = document.createTextNode(upper+ '-\n');
 
-        span.appendChild(text);
-        var div=document.getElementById("results");
-        div.appendChild(span);
-        i++;
+            span.appendChild(text);
+            var div=document.getElementById("results");
+            div.appendChild(span);
+            i++;
         }
     }
     catch(error){
