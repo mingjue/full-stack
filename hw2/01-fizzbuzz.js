c054e758e8fb17a5213e4f0e2381436a07033213
Mingjue <mingjue@vt.edu>
2020-07-12 17:32:18 -0700

    Q2 done

--- hw2/01-fizzbuzz.js
+++ hw2/01-fizzbuzz.js
@@ -5,13 +5,13 @@ function fizzbuzz(){
             document.write(" fizz");
             document.write("<br>");
         }
-     else if( i % 5 == 0){
-        document.write(" buzz");
-        document.write("<br>");
+        else if( i % 5 == 0){
+            document.write(" buzz");
+            document.write("<br>");
         }
-     else if( i % 15 == 0){
-        document.write(" fizzbuzz");
-        document.write("<br>");
+        else if( i % 15 == 0){
+            document.write(" fizzbuzz");
+            document.write("<br>");
         }
         else{
             document.write(i);
--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,1 +1,14 @@
 // Enter your code here
+function reverseinput(){
+    var input =document.getElementById("input").value;  
+    var express = new RegExp('^\\d+$');
+
+    
+    if(input.match(express)){
+        var reverseinput = input.split('').reverse().join("");
+        alert("Input: "+ input +"\nOutput: " + reverseinput);
+    }
+    else{
+        alert("something wrong");
+    }
+}
