ccf69d2fa4f63a24ba0cdb8b9ec4e159ba99100e
Mingjue <mingjue@vt.edu>
2020-07-18 21:24:45 -0700

    fix the small issues

--- hw2/01-fizzbuzz.js
+++ hw2/01-fizzbuzz.js
@@ -1,16 +1,16 @@
 // Enter your code here
 function fizzbuzz(){
     for(var i = 1; i<=100; i++ ){
-        if(i % 3==0){
-            document.write(" fizz");
+        if(i % 15==0){
+            document.write(" fizzbuzz");
             document.write("<br>");
         }
         else if( i % 5 == 0){
             document.write(" buzz");
             document.write("<br>");
         }
-        else if( i % 15 == 0){
-            document.write(" fizzbuzz");
+        else if( i % 3 == 0){
+            document.write(" fizz");
             document.write("<br>");
         }
         else{
--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -2,13 +2,17 @@
 function reverseinput(){
     var input =document.getElementById("input").value;  
     var express = new RegExp('^\\d+$');
-
     
-    if(input.match(express)){
-        var reverseinput = input.split('').reverse().join("");
-        alert("Input: "+ input +"\nOutput: " + reverseinput);
+    if(input.toString().length== 8){
+        if(input.match(express)){
+            var reverseinput = input.split('').reverse().join("");
+            alert("Input: "+ input +"\nOutput: " + reverseinput);
+        }
+        else{
+            alert("something wrong");
+        }
     }
     else{
-        alert("something wrong");
+        alert("Need 8 digit number");
     }
 }
