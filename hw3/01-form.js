ca6acc0d2912d5a73729c1f819e7b4458aa8511f
Mingjue <mingjue@vt.edu>
2020-07-22 14:50:19 -0700

    Q1 done

--- hw3/01-form.js
+++ hw3/01-form.js
@@ -1,1 +1,24 @@
 // Enter your code here
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res)=> {
+
+    res.sendFile(__dirname+'/'+'index.html');
+    
+
+})
+
+app.post('/submit',(req,res)=>{
+    res.status(200);
+    res.send("name :"+ req.body.name+'</br>'+ "email: "+req.body.email+'</br>'+ "comment :"+req.body.else) ;
+})
+
+
+
+app.listen(port,()=>{
+    console.log('server runing at http://localhost:8080');
+});
