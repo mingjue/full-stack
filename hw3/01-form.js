e9400fdcbc16a72d79b35015a8cff9ed8d85b542
Mingjue <mingjue@vt.edu>
2020-08-03 23:02:54 -0700

    update readme

--- hw3/01-form.js
+++ hw3/01-form.js
@@ -11,7 +11,22 @@ app.get('/', (req, res)=> {
 
 app.post('/submit',(req,res)=>{
     res.status(200);
-    res.send("name :"+ req.body.name+'</br>'+ "email: "+req.body.email+'</br>'+ "comment :"+req.body.else) ;
+    if (!req.body.else) {
+        comments = "n/a";
+    } else {
+        comments = req.body.else;
+    }
+    var newsletter;
+    console.log(`what's the value ${req.body.checkbox}`)
+    if (req.body.checkbox) {
+        console.log(`im at checkbox`)
+        
+        newsletter = "Yes, I would like to sign up for the newsletter.";
+    } else {
+        console.log(`im at else checkbox`)
+        newsletter = "No, thank you.";
+    }
+    res.send("name :"+ req.body.name+'</br>'+ "email: "+req.body.email+'</br>'+ "comments :"+comments +"<br />" +"Newsletter: " +newsletter) ;
 })
 
 
